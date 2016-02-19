fa55371b1049ca3f5ad3db2f6335173bf863736b
CTinRay <b03902072@ntu.edu.tw>
2016-02-19 13:52:56 +0800

    using network_config module now

--- manifests/10-controller.pp
+++ manifests/10-controller.pp
@@ -1,50 +1,20 @@
 # $domain = "cloud.csie.ntu.edu.tw" which is got from creator's domain!! 
 
+
 node /controller[1-3]/{
 
 
   # Network interface setting
   
-  $subnets = hiera('subnets')
+ 
   $host_type = $hostname.match(/^[a-z]*/)[0]
-  $ip = hiera('ip_ranges')[$host_type] + $hostname.match(/\d$/)[0]
   
-  notify{ 'hostname':
-    message => " Hostname: ${hostname}, Host Type: ${host_type}, IP surfix: $ip"
-  }
-
-  class { 'network::global':
-    vlan => 'yes',
+  class { '::network_config':
+    ip_ranges => hiera('ip_ranges'),
+    subnets => hiera('subnets'),
+    interface_lists => hiera('interface_lists')
   }
   
-  network::if::static { 'eno1':
-    ensure    => 'up',
-    dns1 => hiera('dns')[0],
-    peerdns => true,
-    domain => $domain,
-    ipaddress => "${subnets['admin']['prefix']}.${ip}",
-    netmask   => $subnets['admin']['mask']
-  }
-  
-  network::if::static { 'eno2':
-    ensure => 'up',
-    ipaddress => "${subnets['external']['prefix']}.${ip}",
-    netmask   => $subnets['external']['mask'],
-    gateway => $subnets['external']['gateway']
-  }
-
-  network::if::static { 'eno2.42':
-    ensure => 'up',
-    ipaddress => "${subnets['manage']['prefix']}.${ip}",
-    netmask   => $subnets['manage']['mask']
-  }
-
-  network::if::static { 'eno2.41':
-    ensure => 'up',
-    ipaddress => "${subnets['storage']['prefix']}.${ip}",
-    netmask   => $subnets['storage']['mask']
-  }
-
   include ::controller_node
 }
 
